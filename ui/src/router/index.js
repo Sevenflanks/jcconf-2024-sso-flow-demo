a5d3b1b415fab8169a240fb8009c775b6db693a0
Rhys <rhys.chang@softleader.com.tw>
2024-09-26 18:04:03 +0800

    移除多餘的註解

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -20,9 +20,6 @@ const router = createRouter({
     {
       path: '/jcconf-demo',
       name: 'jcconf-demo',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/JcconfDemoView.vue')
     }
   ]
